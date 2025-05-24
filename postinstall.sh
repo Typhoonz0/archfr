3273fedcef8475d1c370947e25a5a6f3b244bfea
Typhoonz0 <coolchild1230@gmail.com>
2025-05-24 12:01:29 +1000

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -57,28 +57,27 @@ default() {
 
     cp .zshrc ~/
 
-  #  --- Firefox binary not installed yet, appearently? ---
-  #  git clone https://github.com/Typhoonz0/archfr
-  #  for file in archfr/firefox-ext*; do
-  #      firefox "$file"
-  #  done
-
-  #  curl -LO https://github.com/catppuccin/vscode/releases/download/catppuccin-vsc-v3.17.0/catppuccin-vsc-3.17.0.vsix
-  #  code --install-extension catppuccin-vsc-3.17.0.vsix
-
-  #  curl -LO https://extensions.gnome.org/extension-data/dash-to-dockmicxgx.gmail.com.v71.shell-extension.zip
-  #  curl -LO https://github.com/aunetx/blur-my-shell/releases/download/v68-2/blur-my-shell@aunetx.shell-extension.zip
-  #  curl -LO https://github.com/tiagoporsch/restartto/releases/download/8/restartto@tiagoporsch.github.io.shell-extension.zip
-  #  gnome-extensions install ./custom-hot-corners-extendedG-dH.github.com.v11.shell-extension.zip
-  #  gnome-extensions install ./dash-to-dockmicxgx.gmail.com.v71.shell-extension.zip
-  #  gnome-extensions install ./restartto@tiagoporsch.github.io.shell-extension.zip
-
-  #  gsettings set org.gnome.desktop.background picture-uri file:///~/.config/hypr/dunes.jpg
-  #  gsettings set org.gnome.desktop.wm.preferences button-layout :minimize,maximize,close
-  #  gsettings set org.gnome.shell.extensions.system-monitor show-cpu true
-  #  gsettings set org.gnome.shell.extensions.system-monitor show-memory true
-  #  gsettings set org.gnome.shell.extensions.system-monitor show-swap true
-
+    #  --- Firefox binary not installed yet, appearently? Hopefully this works ---
+    git clone https://github.com/Typhoonz0/archfr
+    for file in archfr/firefox-ext*; do
+        firefox "$file"
+    done
+    
+    curl -LO https://github.com/catppuccin/vscode/releases/download/catppuccin-vsc-v3.17.0/catppuccin-vsc-3.17.0.vsix
+    code --install-extension catppuccin-vsc-3.17.0.vsix
+    
+    curl -LO https://extensions.gnome.org/extension-data/dash-to-dockmicxgx.gmail.com.v71.shell-extension.zip
+    curl -LO https://github.com/aunetx/blur-my-shell/releases/download/v68-2/blur-my-shell@aunetx.shell-extension.zip
+    curl -LO https://github.com/tiagoporsch/restartto/releases/download/8/restartto@tiagoporsch.github.io.shell-extension.zip
+    gnome-extensions install ./custom-hot-corners-extendedG-dH.github.com.v11.shell-extension.zip
+    gnome-extensions install ./dash-to-dockmicxgx.gmail.com.v71.shell-extension.zip
+    gnome-extensions install ./restartto@tiagoporsch.github.io.shell-extension.zip
+    
+    gsettings set org.gnome.desktop.background picture-uri file:///~/.config/hypr/dunes.jpg
+    gsettings set org.gnome.desktop.wm.preferences button-layout :minimize,maximize,close
+    gsettings set org.gnome.shell.extensions.system-monitor show-cpu true
+    gsettings set org.gnome.shell.extensions.system-monitor show-memory true
+    gsettings set org.gnome.shell.extensions.system-monitor show-swap true
     cd ~ && rm -rf archfr yay-bin blur-my-shell@aunetx.shell-extension.zip dash-to-dockmicxgx.gmail.com.v71.shell-extension.zip catppuccin-vsc-3.17.0.vsix
 
 }
